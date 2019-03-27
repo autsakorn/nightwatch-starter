b01e9ad7dcce2c6db034147fb783a73ccec6499b
autsakornt <autsakorn.ta@easysunday.com>
2019-03-27 15:28:30 +0700

    Update config

--- tests/welcome.js
+++ tests/welcome.js
@@ -1,8 +1,13 @@
 module.exports = {
   'Demo google' : function (browser) {
     browser
-      .url('http://google.com')
-      .waitForElementVisible('body', 2000)
+      .url('https://www.google.com')
+      .waitForElementVisible('body')
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('input[name=btnK]')
+      .click('input[name=btnK]')
+      .pause(1000)
+      .assert.containsText('#main', 'Night Watch')
       .end();
   }
 };
